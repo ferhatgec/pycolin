898b4bdfe1da0a66f58c74da44fac5d148fe2563
ferhatgec <ferhat2gecdogan_fix_me5@protonmail.com>
2021-04-02 13:17:06 +0300

    Use: IntEnum instead of Enum & Add rainbow colors.

--- colin.py
+++ colin.py
@@ -17,7 +17,7 @@ import enum
 from sys import argv, exit
 
 
-class InfoType(enum.Enum):
+class InfoType(enum.IntEnum):
     Name = 0
     Hmm = 1
     Rgb = 2
@@ -25,28 +25,46 @@ class InfoType(enum.Enum):
     Cmyk = 4
     Hsl = 5
     Hsv = 6
-
-
-infos = (
-    "name: ",
-    "-----",
-    "\033[0;31mr\033[0;32mg\033[0;34mb\033[0m  : ",
-    "hex  : ",
-    "cmyk : ",
-    "hsl  : ",
-    "hsv  : "
-)
+    Hmm2 = 7
+    Ascii = 8
+    Esc = 9
 
 class Colin:
     color = '\033[48;2;'
-    color_data = color
+    fg_color = '\033[38;2;'
     reset = '\033[0m'
+    hex = ''
+    color_data = color
+
     table_item = '░░'
     line = 0
+
+    infos = [
+        'name: ',
+        '-----',
+        '\033[0;31mr\033[0;32mg\033[0;34mb\033[0m  : ',
+        'hex  : ',
+        'cmyk : ',
+        'hsl  : ',
+        'hsv  : ',
+        '-----',
+        'ascii: ',
+        'esc  : '
+    ]
 
     def __init__(self):
         self.light_gray = self.SetColor(171, 171, 171)
         self.white = self.SetColor(255, 255, 255)
+
+        
+        # Rainbow colors
+        self.red        = self.SetFgColor(255, 0  , 0)
+        self.orange     = self.SetFgColor(255, 165, 0)
+        self.yellow     = self.SetFgColor(255, 255, 0)
+        self.green      = self.SetFgColor(0  , 128, 0)
+        self.blue       = self.SetFgColor(0  , 0, 255)
+        self.purple     = self.SetFgColor(75 , 0, 130)
+        self.pink       = self.SetFgColor(238,130,238)
 
     def name_function(self):
         pass
@@ -69,23 +87,42 @@ class Colin:
     def hsv_function(self):
         pass
 
+    def hmm2_function(self):
+        pass
+
+    def ascii_function(self):
+        pass
+
+    def esc_function(self):
+        pass
+
     def switch(self, arg: InfoType):
         {
             InfoType.Name: self.name_function(),
+
             InfoType.Hmm: self.hmm_function(),
+
             InfoType.Rgb: self.rgb_function(),
             InfoType.Hex: self.hex_function(),
             InfoType.Cmyk: self.cmyk_function(),
             InfoType.Hsl: self.hsl_function(),
-            InfoType.Hsv: self.hsv_function()
+            InfoType.Hsv: self.hsv_function(),
+
+            InfoType.Hmm2: self.hmm2_function(),
+
+            InfoType.Ascii: self.ascii_function(),
+            InfoType.Esc: self.esc_function()
         }[arg]
 
     def SetColor(self, r: int, g: int, b: int) -> str:
         return self.color + str(r) + ';' + str(g) + ';' + str(b) + 'm'
 
+    def SetFgColor(self, r: int, g: int, b: int) -> str:
+        return self.fg_color + str(r) + ';' + str(g) + ';' + str(b) + 'm'
+
     def Newline(self):
-        if self.line < len(infos):
-            print(' ', infos[self.line])
+        if self.line < len(self.infos):
+            print(' ', self.infos[self.line])
 
             self.switch(InfoType(self.line))
         else:
@@ -112,6 +149,12 @@ class Colin:
     def Init(self, r: int, g: int, b: int):
         self.color_data = self.SetColor(r, g, b)
 
+        self.infos[InfoType.Hex] = (self.red + 'hex  : ' + self.orange + self.hex)
+        self.infos[InfoType.Cmyk] = (self.orange + 'cmyk : ' + self.yellow + 'work-in-progress')
+        self.infos[InfoType.Hsl] = (self.yellow + 'hsl  : ' + self.green + 'work-in-progress')
+        self.infos[InfoType.Hsv] = (self.green + 'hsv  : ' + self.blue + 'work-in-progress')
+        self.infos[InfoType.Ascii] = (self.blue + 'ascii: ' + self.purple + 'work-in-progress')
+        self.infos[InfoType.Esc] = (self.purple + 'esc  : ' + self.pink + 'work-in-progress')
 
     def PrintColorBox(self, split: bool):
         if split:
